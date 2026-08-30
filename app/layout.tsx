892d82dbb50a9b15ea1ecdc1c9525317c37f64a6
Vratesh Ghadge <ghadgevratesh@gmail.com>
2026-08-30 18:56:59 +0530

    feat: add toaster component to layout and page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google"
+import { Toaster } from "@/components/ui/sonner"
 
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
@@ -32,6 +33,7 @@ export default function RootLayout({
     >
       <body suppressHydrationWarning>
         <ThemeProvider>{children}</ThemeProvider>
+        <Toaster />
       </body>
     </html>
   )
